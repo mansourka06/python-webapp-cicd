04d77c313435cfe3ec86546939d67a51d92a81b9
Mansour KA <52289882+mansourka06@users.noreply.github.com>
2024-03-20 15:47:04 +0100

    update ci

--- app/app.py
+++ app/app.py
@@ -8,6 +8,4 @@ def hello():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000)
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    app.run(host='0.0.0.0', port=5000)
